c3b79dbe5a8cb4804f2059e6e2f81dbc5ffc348b
Alex Soroka <lehaSVV2009@gmail.com>
2018-11-11 14:27:36 +0300

    Complete the tutorial of boardgame.io with tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
+import { Client } from "boardgame.io/react";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            ß Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import AI from "./AI";
+import Board from "./Board";
+import Game from "./Game";
+
+const App = Client({ game: Game, board: Board, ai: AI });
 
 export default App;
