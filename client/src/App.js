e88171df80e844630f7884830e6e3208098c9286
Alex Soroka <lehaSVV2009@gmail.com>
2018-11-16 01:48:20 +0300

    Move client app to a separate sub-folder

---
+++ client/src/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Client } from "boardgame.io/react";
+
+import AI from "./AI";
+import Board from "./Board";
+import Game from "./Game";
+
+const TicTacToeSinglePlayer = Client({
+  game: Game,
+  board: Board,
+  ai: AI
+});
+
+// const TicTacToeMultiPlayerLocal = Client({
+//   game: TicTacToe,
+//   board: Board,
+//   multiplayer: { local: true }
+// });
+
+// const TicTacToeMultiPlayerRemote = Client({
+//   game: TicTacToe,
+//   board: Board,
+//   multiplayer: { server: "http://localhost:8000" }
+// });
+
+const App = () => (
+  <div>
+    <div>
+      Single player
+      <TicTacToeSinglePlayer />
+    </div>
+    {/* <div>
+      Multi player local
+      <TicTacToeMultiPlayerLocal playerID="0" />
+      <TicTacToeMultiPlayerLocal playerID="1" />
+    </div>
+    <div>
+      Multi player remote
+      <TicTacToeMultiPlayerRemote playerID="0" />
+      <TicTacToeMultiPlayerRemote playerID="1" />
+    </div> */}
+  </div>
+);
+
+export default App;
--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { Client } from "boardgame.io/react";
-
-import AI from "./AI";
-import Board from "./Board";
-import Game from "./Game";
-
-const App = Client({ game: Game, board: Board, ai: AI });
-
-export default App;
