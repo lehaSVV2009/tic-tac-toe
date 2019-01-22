0fb6a7818e79f56ff6ac0a4d2408876f1ede3b19
Alex Soroka <lehaSVV2009@gmail.com>
2019-01-22 23:52:09 +0530

    Try not to just fetch, but also create games

--- src/App.js
+++ src/App.js
@@ -25,11 +25,24 @@ const TicTacToeMultiPlayerRemote = Client({
 
 class App extends React.Component {
   componentDidMount() {
-    axios
+    this.createGame("default").finally(() => this.fetchGames());
+  }
+
+  createGame = name => {
+    return axios
+      .post(`${process.env.REACT_APP_API_URL}/games/${name}/create`, {})
+      .then(response => {
+        alert(`Create game ${name} response: ${JSON.stringify(response.data)}`);
+      })
+      .catch(error => alert(`Create game ${name} error: ${error}`));
+  };
+
+  fetchGames = () => {
+    return axios
       .get(`${process.env.REACT_APP_API_URL}/games`)
       .then(response => alert(`Fetch games response: ${response.data}`))
       .catch(error => alert(`Fetch games error: ${error}`));
-  }
+  };
 
   render() {
     return (
