1b3f09654adc83025b821047279ffbe90a81abd7
Alex Soroka <lehaSVV2009@gmail.com>
2019-01-25 23:12:48 +0530

    Use boardgame.io lobby as a multiplayer game start ui

--- src/App.js
+++ src/App.js
@@ -1,69 +1,18 @@
 import React from "react";
-import axios from "axios";
-import { Client } from "boardgame.io/react";
 import { Game } from "tic-tac-toe-core";
+import Lobby from "./libs/boardgame.io/react";
+import "./lobby.css";
 
 import Board from "./Board";
 
-// const TicTacToeSinglePlayer = Client({
-//   game: Game,
-//   board: Board,
-//   ai: AI
-// });
+Game.minPlayers = Game.maxPlayers = 2;
 
-// const TicTacToeMultiPlayerLocal = Client({
-//   game: TicTacToe,
-//   board: Board,
-//   multiplayer: { local: true }
-// });
-
-const TicTacToeMultiPlayerRemote = Client({
-  game: Game,
-  board: Board,
-  multiplayer: { server: process.env.REACT_APP_API_URL }
-});
-
-class App extends React.Component {
-  componentDidMount() {
-    this.createGame(Game.name).finally(() => this.fetchGames());
-  }
-
-  createGame = name => {
-    return axios
-      .post(`${process.env.REACT_APP_API_URL}/games/${name}/create`, {})
-      .then(response => {
-        alert(`Create game ${name} response: ${JSON.stringify(response.data)}`);
-      })
-      .catch(error => alert(`Create game ${name} error: ${error}`));
-  };
-
-  fetchGames = () => {
-    return axios
-      .get(`${process.env.REACT_APP_API_URL}/games`)
-      .then(response => alert(`Fetch games response: ${response.data}`))
-      .catch(error => alert(`Fetch games error: ${error}`));
-  };
-
-  render() {
-    return (
-      <div>
-        {/* <div>
-        Single player
-        <TicTacToeSinglePlayer />
-      </div> */}
-        {/* <div>
-        Multi player local
-        <TicTacToeMultiPlayerLocal playerID="0" />
-        <TicTacToeMultiPlayerLocal playerID="1" />
-      </div> */}
-        <div>
-          Multi player remote
-          <TicTacToeMultiPlayerRemote playerID="0" />
-          <TicTacToeMultiPlayerRemote playerID="1" />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Lobby
+    gameServer={process.env.REACT_APP_API_URL}
+    lobbyServer={process.env.REACT_APP_API_URL}
+    gameComponents={[{ game: Game, board: Board }]}
+  />
+);
 
 export default App;
