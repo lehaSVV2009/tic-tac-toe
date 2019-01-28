bdb1ccf01e865310ddbb367da1a57c9f93b5561f
Alex Soroka <lehaSVV2009@gmail.com>
2019-01-29 00:17:50 +0530

    Add lobby custom renderer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Game } from "tic-tac-toe-core";
 import Lobby from "./libs/boardgame.io/react";
-import "./lobby.css";
 
 import Board from "./Board";
+import StyledLobby from "./StyledLobby";
 
 Game.minPlayers = Game.maxPlayers = 2;
 
@@ -12,6 +12,37 @@ const App = () => (
     gameServer={process.env.REACT_APP_API_URL}
     lobbyServer={process.env.REACT_APP_API_URL}
     gameComponents={[{ game: Game, board: Board }]}
+    renderer={({
+      errorMsg,
+      gameComponents,
+      gameInstances,
+      phase,
+      playerName,
+      runningGame,
+      onEnterLobby,
+      onExitLobby,
+      onCreateRoom,
+      onJoinRoom,
+      onLeaveRoom,
+      onExitRoom,
+      onStartGame
+    }) => (
+      <StyledLobby
+        errorMsg={errorMsg}
+        gameComponents={gameComponents}
+        gameInstances={gameInstances}
+        phase={phase}
+        playerName={playerName}
+        runningGame={runningGame}
+        onEnterLobby={onEnterLobby}
+        onExitLobby={onExitLobby}
+        onCreateRoom={onCreateRoom}
+        onJoinRoom={onJoinRoom}
+        onLeaveRoom={onLeaveRoom}
+        onExitRoom={onExitRoom}
+        onStartGame={onStartGame}
+      />
+    )}
   />
 );
 
