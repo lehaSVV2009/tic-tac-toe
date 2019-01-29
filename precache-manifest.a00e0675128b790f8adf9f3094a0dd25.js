0e214020354b58609669dbee13deaf6c31e68081
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-29 10:49:46 +0000

    Deploy lehaSVV2009/tic-tac-toe-client to github.com/lehaSVV2009/tic-tac-toe-client.git:gh-pages

---
+++ precache-manifest.a00e0675128b790f8adf9f3094a0dd25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24f5e6cc582e7d6c6805",
+    "url": "/tic-tac-toe-client/static/js/runtime~main.24f5e6cc.js"
+  },
+  {
+    "revision": "4e612cd528238f1295be",
+    "url": "/tic-tac-toe-client/static/js/main.4e612cd5.chunk.js"
+  },
+  {
+    "revision": "dd1f1822c2bc4183f95d",
+    "url": "/tic-tac-toe-client/static/js/1.dd1f1822.chunk.js"
+  },
+  {
+    "revision": "4e612cd528238f1295be",
+    "url": "/tic-tac-toe-client/static/css/main.8bd7816c.chunk.css"
+  },
+  {
+    "revision": "feec4d425042459d84e0a2bb1249b879",
+    "url": "/tic-tac-toe-client/index.html"
+  }
+];
